cc767000b9dadc0ed01590a7837dd4a5ba59b486
devedux <devedux@gmail.com>
2021-02-27 15:34:58 -0500

    first commit of de project

---
+++ pages/_document.js
@@ -0,0 +1,58 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet, createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+    body {
+        padding: 0;
+        margin: 0;
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
+`
+class MyDocument extends Document {
+    static async getInitialProps(ctx) {
+        const sheet = new ServerStyleSheet()
+        const originalRenderPage = ctx.renderPage
+        try {
+            ctx.renderPage = _ => 
+                originalRenderPage({
+                    enhanceApp: App => props => 
+                    sheet.collectStyles(
+                        <>
+                            <GlobalStyle />
+                            <App {...props} />
+                        </>
+                    ),
+                    enhanceComponent: Component => Component,
+                })
+
+                const initialProps = await Document.getInitialProps(ctx)
+
+                return {
+                    ...initialProps,
+                    styles: (
+                        <>
+                            {initialProps.styles}
+                            {sheet.getStyleElement()}
+                        </>
+                    )
+                }
+
+        } finally {
+            sheet.seal()
+        }
+    }
+
+    render() {
+        return (
+            <Html lang='es' >
+                <Head />
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
+
+export default MyDocument
